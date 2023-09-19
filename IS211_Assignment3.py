e9c46f30a08516a58839f4d983082034d70df53a
cajas1927 <123519445+cajas1927@users.noreply.github.com>
2023-09-18 21:02:14 -0400

    Add files via upload

---
+++ IS211_Assignment3.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import re
+
+def image_format_func(text):
+    image_format = ['.jpg', '.png', '.gif']
+    for i in image_format:
+        if bool(re.search(i , text)) == True:
+            return True
+    return False
+
+def browser_func(text):
+    if bool(re.search('Firefox' , text)) == True:
+        return 'Mozilla Firefox'
+    elif bool(re.search('Chrome' , text)) == True:
+        return 'Google Chrome'
+    elif bool(re.search('Explorer' , text)) == True:
+        return 'Internet Explorer'
+    elif bool(re.search('Safari' , text)) == True:
+        return 'Safari'
+    
+def hour_func(text):
+    return list(text.split())[-1][:2]
+
+#PART II
+df = pd.read_csv('birthdays100.csv')
+
+
+#PART III
+
+df['image_bool'] = df['path_to_file'].apply(lambda x: image_format_func(x), axis = 1)
+
+
+#Find out the percentage for it and print its value
+percentage = (len(df[df['image_bool']==True])/len(df['image_bool']))*100
+print('Image requests account for {} of all requests'.format(percentage))
+
+#PART IV
+
+df['browser'] = df['browser'].apply(lambda x: browser_func(x), axis = 1)
+
+print('Browsers with maximum occurences: ', df['browser'].mode().to_list())
+
+#PART VI
+#Now the extra credits question:
+df['hour'] = df['datetime_accessed'].apply(lambda x: hour_func(x), axis = 1)
+
+hour_dict = df['hour'].value_counts().to_dict()
+
+# printing the key-value pairs of the dictionary in the given format
+
+for hour, hits in hour_dict.items():
+    print('Hour {} has {} hits'.format(hour, hits))
